d0cc75409ddc38bb2d4059e1806ec8e98c205efa
Loren Glenn and Maxwell Cady <maxwell@>
2016-10-19 11:06:19 -0700

    add answer section at bottom of screen

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,19 @@ $(function() {
   $("form#calculator").submit(function(event) {
     var firstNumber = parseInt($("input#firstNumber").val());
     var secondNumber = parseInt($("input#secondNumber").val());
+    var result;
 
-     if(operator === "+") {
-        alert(firstNumber + secondNumber)
-      } else if(operator === "-") {
-        alert(firstNumber - secondNumber)
-      } else if(operator === "*") {
-        alert(firstNumber * secondNumber)
-      } else if(operator === "/") {
-        alert(firstNumber / secondNumber)
-      };
+    if(operator === "+") {
+      result = (firstNumber + secondNumber)
+    } else if(operator === "-") {
+      result = (firstNumber - secondNumber)
+    } else if(operator === "*") {
+      result = (firstNumber * secondNumber)
+    } else if(operator === "/") {
+      result = (firstNumber / secondNumber)
+    };
 
+    $("div#result").text(result);
 
     event.preventDefault();
   });
