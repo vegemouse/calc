e1d675f00435cb2a6f2af4ad79c71ad8dc49be46
Loren Glenn and Maxwell Cady <loren@>
2016-10-19 13:04:06 -0700

    add gallons to liters function and front end

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,42 @@
 $(function() {
+  var gallons, liters, firstNumber, secondNumber, result;
   var operator = $("input.operator").click(function() {
     operator =  $(this).val();
   });
+
+  var calculate = function(firstNumber, secondNumber) {
+    if(operator === "+") {
+      result = (firstNumber + secondNumber);
+    } else if(operator === "-") {
+      result = (firstNumber - secondNumber);
+    } else if(operator === "*") {
+      result = (firstNumber * secondNumber);
+    } else if(operator === "/") {
+      result = (firstNumber / secondNumber);
+    };
+  };
+
+  var galsToLiters = function(gallons) {
+    liters = (gallons * 3.78541);
+  }
+
+// begin front end logic
+
   $("form#calculator").submit(function(event) {
     var firstNumber = parseInt($("input#firstNumber").val());
     var secondNumber = parseInt($("input#secondNumber").val());
-    var result;
-
-    if(operator === "+") {
-      result = (firstNumber + secondNumber)
-    } else if(operator === "-") {
-      result = (firstNumber - secondNumber)
-    } else if(operator === "*") {
-      result = (firstNumber * secondNumber)
-    } else if(operator === "/") {
-      result = (firstNumber / secondNumber)
-    };
-
-    $("div#result").text(result);
+    calculate(firstNumber, secondNumber);
+    $("div#calc-result").text(result);
 
     event.preventDefault();
   });
+
+  $("form#cooking-gallons").submit(function(event){
+    var gallons = parseInt($("input#gallons").val());
+    galsToLiters(gallons);
+    $("div#liters").text(liters + " liters");
+
+
+    event.preventDefault();
+  })
 });
